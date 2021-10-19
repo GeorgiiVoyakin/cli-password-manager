d61c709ef95c8a9b823acb2be0df23e1e8c2f71d
Георгий Воякин <voyaking@gmail.com>
2021-10-19 20:23:16 +0300

    Changed get master password to verify master password

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use ring::digest;
+use ring::error;
 use ring::pbkdf2;
 use ring::rand;
 use ring::rand::SecureRandom;
 use std::fs::File;
 use std::io::prelude::*;
 use std::num::NonZeroU32;
+use std::result;
 
 fn encode_password(password: &str) -> String {
     base64::encode(password)
 }
-fn decode_password(encoded: &[u8]) -> Result<Vec<u8>, base64::DecodeError> {
+fn decode_password(encoded: &[u8]) -> result::Result<Vec<u8>, base64::DecodeError> {
     base64::decode(encoded)
 }
 
@@ -17,6 +19,11 @@ fn save_master_password(master_password: &[u8]) -> std::io::Result<()> {
     let mut file = File::create("password.txt")?;
     file.write_all(master_password)?;
     Ok(())
+}
+
+fn verify_master_password(password_to_verify: &str, n_iter: NonZeroU32, salt: &[u8], pbkdf2_hash: &mut [u8]) -> result::Result<(), error::Unspecified> {
+    let file = File::open("master_password.txt");
+    pbkdf2::verify(pbkdf2::PBKDF2_HMAC_SHA512, n_iter, salt, password_to_verify.as_bytes(), pbkdf2_hash)
 }
 
 fn main() {
@@ -43,8 +50,9 @@ fn main() {
         &mut pbkdf2_hash,
     );
 
-    let mut file = File::open("master_password.txt");
-    file.write(&pbkdf2_hash, &salt).unwrap();
+    let mut file = File::open("master_password.txt").unwrap();
+    file.write(&pbkdf2_hash).unwrap();
+    file.write(&salt).unwrap();
 }
 
 #[test]
@@ -64,6 +72,18 @@ fn test_save_master_password() -> std::io::Result<()> {
 }
 
 #[test]
-fn test_get_master_password() {
-    assert_eq!("password", get_master_password());
+fn test_verify_master_password() {
+    const CREDENTIAL_LEN: usize = digest::SHA512_OUTPUT_LEN;
+    let n_iter = NonZeroU32::new(100_000).unwrap();
+    let mut salt = [0u8; CREDENTIAL_LEN];
+    let mut pbkdf2_hash = [0u8; CREDENTIAL_LEN];
+
+    pbkdf2::derive(
+        pbkdf2::PBKDF2_HMAC_SHA512,
+        n_iter,
+        &salt,
+        b"password",
+        &mut pbkdf2_hash,
+    );
+    assert!(verify_master_password("password", n_iter, &salt, &mut pbkdf2_hash).is_ok());
 }
