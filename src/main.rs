140f5b9047eac7f0d53754b2d35ccfdd83414299
Георгий Воякин <voyaking@gmail.com>
2021-10-19 18:58:23 +0300

    Added tests and encode, decode functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn encode_password(password: &str) -> std::result::Result<(), ()> {
+    todo!();
+}
+fn decode_password(encoded: &[u8]) -> std::result::Result<&str, ()> {
+    todo!();
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn test_encoding() {
+    assert!(encode_password("password").is_ok());
+}
+
+#[test]
+fn test_decoding() {
+    let coded = b"cGFzc3dvcmQ=";
+    assert_eq!(decode_password(coded).unwrap(), "password");
 }
